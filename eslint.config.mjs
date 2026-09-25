9bcb701fa38f62e6c178c5af30506dbe48ff67f8
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2026-09-25 12:46:40 -0400

    Added prettier to project, configured eslint to match prettier formatting settings, configured vscode to format on save

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,21 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
-import betterTailwindcss from 'eslint-plugin-better-tailwindcss'
-import { getDefaultAttributes } from 'eslint-plugin-better-tailwindcss/api/defaults'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import betterTailwindcss from "eslint-plugin-better-tailwindcss";
+import { getDefaultAttributes } from "eslint-plugin-better-tailwindcss/api/defaults";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default withNuxt(
-  betterTailwindcss.configs['correctness-error'],
+  betterTailwindcss.configs["correctness-error"],
   {
     settings: {
-      'better-tailwindcss': {
-        entryPoint: 'app/assets/css/main.css',
+      "better-tailwindcss": {
+        entryPoint: "app/assets/css/main.css",
         attributes: [
           ...getDefaultAttributes(),
-          ['^v-bind:ui$', [{ match: 'objectValues' }]]
-        ]
-      }
-    }
-  }
-)
+          ["^v-bind:ui$", [{ match: "objectValues" }]],
+        ],
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
